8c4ecf918c0cd94a70c2ef9f4e23091206a09fe0
gregr <greg.weiqi@gmail.com>
2020-06-08 17:04:17 -0400

    Create relation.rkt

---
+++ relation.rkt
@@ -0,0 +1,30 @@
+#lang racket/base
+(provide)
+(require "method.rkt")
+
+;; * extensional relation:
+;;   * schema:
+;;     * heading: set of attributes and their types
+;;     * degree constraints (generalized functional dependencies)
+;;     * possibly join and inclusion dependencies
+;;   * body: finite set of tuples
+
+;; bodies, aka data sources:
+;;   * single stream
+;;   OR
+;;   * main table and supporting (index) tables
+;;   OR
+;;   * extension of an existing relation to include an implicit position
+
+;; streams (e.g., csv/tsv files) are not tables, can't efficiently bisect
+;; but prepending a position column to each tuple implicitly sorts them
+
+;; constraints:
+;;   * functional dependency expressed as degree constraint of 1
+;;   * uniqueness expressed as functional dependency
+
+;; Non-prefix columns may be guaranteed to be sorted if they vary monotonically
+;; with the table prefix.  This means the same table to be used as a virtual
+;; index for multiple prefixes.  The virtual table reorders its columns to put
+;; an alternate sorted column first.
+
