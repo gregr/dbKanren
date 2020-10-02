9af1a71522324855ab077793158cdf3d2e32af9e
gregr <greg.weiqi@gmail.com>
2020-10-02 16:51:30 -0400

    Factor out query->stream for easier testing

--- mk.rkt
+++ mk.rkt
@@ -4,11 +4,12 @@
          run^ run run*)
 (require "mk/common.rkt" "mk/naive.rkt" "mk/syntax.rkt" "stream.rkt")
 
-;(define-syntax run^
-  ;(syntax-rules () ((_   body ...) (dfs:query->stream (query  body ...)))))
+;(define query->stream dfs:query->stream)
+(define query->stream bis:query->stream)
+
 (define-syntax run^
-  (syntax-rules () ((_   body ...) (bis:query->stream (query  body ...)))))
+  (syntax-rules () ((_   body ...) (query->stream (query  body ...)))))
 (define-syntax run
-  (syntax-rules () ((_ n body ...) (s-take n          (run^   body ...)))))
+  (syntax-rules () ((_ n body ...) (s-take n      (run^   body ...)))))
 (define-syntax run*
-  (syntax-rules () ((_   body ...)                    (run #f body ...))))
+  (syntax-rules () ((_   body ...)                (run #f body ...))))
