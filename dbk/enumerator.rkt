b7d3a6665751c9b7eaf7c03f4bbd6a9f99905b72
gregr <greg.weiqi@gmail.com>
2021-09-14 21:11:05 -0400

    Rename enumerator yield parameter

--- dbk/enumerator.rkt
+++ dbk/enumerator.rkt
@@ -8,21 +8,21 @@
          hash->enumerator/2)
 (require racket/control racket/unsafe/ops)
 
-(define ((enumerator-append e.0 e.1) k!)
-  (e.0 k!)
-  (e.1 k!))
+(define ((enumerator-append e.0 e.1) yield)
+  (e.0 yield)
+  (e.1 yield))
 
-(define ((list->enumerator xs) k!) (for-each k! xs))
+(define ((list->enumerator xs) yield) (for-each yield xs))
 
 (define (enumerator->list en)
   (define xs '())
   (en (lambda (x) (set! xs (cons x xs))))
   (reverse xs))
 
-(define ((s->enumerator s) k!)
+(define ((s->enumerator s) yield)
   (let loop ((s s))
     (cond ((null? s) (void))
-          ((pair? s) (k! (car s)) (loop (cdr s)))
+          ((pair? s) (yield (car s)) (loop (cdr s)))
           (else      (loop (s))))))
 
 (define ((enumerator->s en))
@@ -36,21 +36,21 @@
   (define len (min end (vector-length v)))
   (unsafe-vector->enumerator v (min start len) len))
 
-(define ((unsafe-vector->enumerator v (start 0) (end (unsafe-vector*-length v))) k!)
+(define ((unsafe-vector->enumerator v (start 0) (end (unsafe-vector*-length v))) yield)
   (let loop ((i start))
     (when (unsafe-fx< i end)
-      (k!   (unsafe-vector*-ref v i))
+      (yield (unsafe-vector*-ref v i))
       (loop (unsafe-fx+ i 1)))))
 
-(define ((generator->enumerator gen stop?) k!)
+(define ((generator->enumerator gen stop?) yield)
   (let loop ()
     (define x (gen))
     (unless (stop? x)
-      (k! x)
+      (yield x)
       (loop))))
 
 ;; An enumerator/2 expects its iteratee to take two arguments
-(define ((enumerator/2->enumerator en) k!)
-  (en (lambda (a b) (k! (cons a b)))))
+(define ((enumerator/2->enumerator en) yield)
+  (en (lambda (a b) (yield (cons a b)))))
 
-(define ((hash->enumerator/2 kvs) k!) (hash-for-each kvs k!))
+(define ((hash->enumerator/2 kvs) yield) (hash-for-each kvs yield))
