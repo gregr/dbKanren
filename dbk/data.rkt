f2138a36fb005f1c21182fc70dddacd062f8d702
gregr <greg.weiqi@gmail.com>
2021-10-30 17:28:59 -0400

    Use bytes-nat-ref

--- dbk/data.rkt
+++ dbk/data.rkt
@@ -404,8 +404,7 @@
 (define ((column:table     columns)                i) (map (lambda (col) (col i)) columns))
 (define ((column:indirect  column.pos column)      i) (column (column.pos i)))
 (define ((column:interval  column.pos interval->x) i) (interval->x (column.pos i) (column.pos (+ i 1))))
-(define ((column:bytes:nat bs size)                i) (let ((i (* i size)))
-                                                        (integer-bytes->integer bs #f #t i (+ i size))))
+(define ((column:bytes:nat bs size)                i) (bytes-nat-ref bs size (* i size)))
 
 (define (column:port                     in type) (let ((size.type (sizeof type (void))))
                                                     (lambda (i)
