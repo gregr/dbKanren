01ac20abe45c3f22712bc0afcece439c6170681a
gregr <greg.weiqi@gmail.com>
2021-11-13 19:03:09 -0500

    Update test-ingest-example-simple.rkt

--- test/test-ingest-example-simple.rkt
+++ test/test-ingest-example-simple.rkt
@@ -1,35 +1,46 @@
 #lang racket/base
 (require "../dbk/io.rkt"
          "../dbk/data.rkt"
-         racket/match)
+         "../dbk/stream.rkt"
+         racket/runtime-path)
 
-(define db (database "path/to/example/db"))
+(define-runtime-path path.here ".")
 
-(define cprop (database-relation-add!
-                db '(example cprop)
-                'attributes '(curie  key    value)
-                'type       '(string string string)
-                'source     (in:file "example/example.nodeprop.tsv" 'header '(":ID" "propname" "value"))))
-(define eprop (database-relation-add!
-                db '(example eprop)
-                'attributes '(eid key    value)
-                'type       '(nat string string)
-                'source     (in:file "example/example.edgeprop.tsv" 'header '(":ID" "propname" "value"))))
-(define edge  (database-relation-add!
-                db '(example edge)
-                'attributes '(eid subject object)
-                'type       '(nat string  string)
-                'source     (in:file "example/example.edge.tsv"     'header '(":ID" ":START" ":END"))))
+(define db (database (path->string (simplify-path (build-path path.here "example-db")))))
+
+(database-relation-add!
+  db '(example cprop)
+  'attributes '(curie  key    value)
+  'type       '(string string string)
+  'source     (in:file "example/example.nodeprop.tsv" 'header '(":ID" "propname" "value")))
+
+(database-relation-add!
+  db '(example edge)
+  'attributes '(eid subject object)
+  'type       '(nat string  string)
+  'source     (s-map (lambda (row) (cons (string->number (car row)) (cdr row)))
+                     (in:file "example/example.edge.tsv"     'header '(":ID" ":START" ":END"))))
+
+(database-relation-add!
+  db '(example eprop)
+  'attributes '(eid key    value)
+  'type       '(nat string string)
+  'source     (s-map (lambda (row) (cons (string->number (car row)) (cdr row)))
+                     (in:file "example/example.edgeprop.tsv" 'header '(":ID" "propname" "value"))))
+
+(define cprop (database-relation '(example cprop)))
+(define edge  (database-relation '(example edge)))
+(define eprop (database-relation '(example eprop)))
 
 (database-compact! db)
 
 (relation-index-add! cprop
                      '(curie key)
                      '(key value))
-(relation-index-add! eprop
-                     '(eid key)
-                     '(key value))
 (relation-index-add! edge
                      '(eid)
                      '(subject object)
                      '(object subject))
+(relation-index-add! eprop
+                     '(eid key)
+                     '(key value))
