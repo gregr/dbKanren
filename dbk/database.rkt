239a83a7e9627c76f57fdf1f03302124fc726eca
gregr <greg.weiqi@gmail.com>
2022-02-25 17:23:48 -0500

    Describe intended database implementation

---
+++ dbk/database.rkt
@@ -0,0 +1,45 @@
+#lang racket/base
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Persistent databases ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Limitations:
+;; - A single database will not scale well to enormous numbers of relations.
+;; - This implementation will not scale well to high frequency database updates.
+;; - A single database should not be used by multiple host processes running concurrently.
+;;   Initializing the same database from concurrent processes may lead to data corruption.
+;; - Multiple threads of a single process can concurrently read from the same database, but it is
+;;   not safe to read concurrently with an update to the same database.
+;; - It is not safe to perform concurrent updates on the same database.
+;; - While it should be possible to recover from typical process failure and interruption, sudden
+;;   host system failure and interruption may corrupt data.
+
+;; A database is a persistent collection of extensional relations, and is represented as a
+;; filesystem directory managed by an instance of checkpointing storage.
+
+;; An extensional relation is a uniquely named, finite set of ordered tuples.  Each tuple in the set
+;; contains the same number of fields.  For each field, every tuple assigns the same name and type
+;; to that field.  The list of field types is called the relation's type.  The field names are
+;; called the relation's attributes, and each name must be unique within the relation.
+
+;; For efficient querying, a relation can be indexed on one or more permutations of a subset of its
+;; attributes.  An index provides an efficient way to filter and enumerate a subset of the
+;; relation's tuples.  To filter the tuples, constraints are applied to a subset of the relation's
+;; attributes, eliminating tuples that do not satisfy the constraints.  The index's attribute
+;; permutation is the order in which attributes will be constrained.  This order of constraining
+;; attributes is efficient due to the representation of the index, which is the result of sorting
+;; the relation's tuples lexicographically according to the attribute permutation.
+
+;; Databases and relations can be modified:
+;; - New relations can be added to, and existing relations can be removed from, a database.
+;; - Relations and their attributes can be renamed.
+;; - Indexes can be added to, or removed from, a relation.
+;; - Tuples can be inserted into, or deleted from, a relation.
+;; Database and relation modifications always occur in the context of an atomic database update, and
+;; multiple such modifications can be performed as part of the same update.  This means that
+;; multiple relations can be modified simultaneously, each being modified in one or more ways,
+;; during such update.  Updates are atomic in the sense that they never partially succeed: either
+;; all specified modifications are performed, or none are performed.
+
+(define version.current '2022-2-22)
