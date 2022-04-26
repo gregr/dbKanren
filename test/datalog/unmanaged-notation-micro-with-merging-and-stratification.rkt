976f631d519df54f281b7aa216089d9601c34515
gregr <greg.weiqi@gmail.com>
2022-04-25 22:01:57 -0400

    Define run-stratified

---
+++ test/datalog/unmanaged-notation-micro-with-merging-and-stratification.rkt
@@ -0,0 +1,31 @@
+#lang racket/base
+(provide run-stratified)
+(require "micro-with-merging.rkt" (except-in racket/match ==))
+
+(define (atom-vars atom) (filter var? atom))
+
+(define (rule-safe?! rule)
+  (let ((vars.body (apply append (map atom-vars (cdr rule)))))
+    (for-each (lambda (var.head) (or (member var.head vars.body)
+                                     (error "unsafe rule" rule)))
+              (atom-vars (car rule)))))
+
+(define (parse-term expr)
+  (match expr
+    ((? symbol?) (var expr))
+    (`(quote ,c) c)
+    ((cons _ _)  (error "unsupported function call" expr))
+    (_           expr)))
+
+(define (parse-atom expr) (cons (car expr) (map parse-term (cdr expr))))
+(define (parse-rule expr) (let ((rule (map parse-atom expr)))
+                            (rule-safe?! rule)
+                            rule))
+
+(define (enforce rule) (realize (car rule) (conj* (map relate (cdr rule)))))
+
+(define (run-stratified predicate=>merge e**.rules F*)
+  (foldr (lambda (p* F*) (exhaust* p* predicate=>merge F*))
+         F*
+         (map (lambda (e*.rules) (map enforce (map parse-rule e*.rules)))
+              e**.rules)))
