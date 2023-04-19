e6420d698fc035ca9b2095390a0161f91812595b
gregr <greg.weiqi@gmail.com>
2023-04-19 17:07:05 -0400

    Define more stream operators

--- dbk/stream.rkt
+++ dbk/stream.rkt
@@ -1,12 +1,14 @@
 #lang racket/base
 (provide s-next s-force s-split s-drop
          s-take/set/steps s-take/set s-take/steps s-take
-         s-each s-foldr s-foldl s-scan
+         s-each s-foldr s-foldl s-split-foldl s-scan
          s-append/interleaving s-append*/interleaving
          s-append s-append* s-map/append s-map s-filter s-group s-memo s-lazy
          s-length s-enumerate s-dedup s-skip s-limit
-         s->list list->s)
-(require racket/function racket/match racket/set)
+         s-chunk s-unchunk s->list list->s)
+(require ;"safe-unsafe.rkt"
+         racket/unsafe/ops
+         racket/function racket/match racket/set racket/vector)
 
 (define (s-next  s) (if (procedure? s)          (s)  s))
 (define (s-force s) (if (procedure? s) (s-force (s)) s))
@@ -22,9 +24,22 @@
         ((pair? s)              (cons (car s) (s-limit (- n 1) (cdr s))))
         (else                   (thunk (s-limit n (s))))))
 
+(define (s-foldl f acc s)
+  (let loop ((acc acc) (s s))
+    (cond ((null?      s) acc)
+          ((procedure? s) (loop acc (s)))
+          (else           (loop (f (car s) acc) (cdr s))))))
+
+(define (s-split-foldl n f acc s)
+  (let loop ((n n) (acc acc) (s s))
+    (cond ((= n 0)        (values acc s))
+          ((null?      s) (values acc '()))
+          ((procedure? s) (loop n acc (s)))
+          (else           (loop (- n 1) (f (car s) acc) (cdr s))))))
+
 (define (s-split n s)
-  (match-define (cons rxs s.remaining) (s-foldl n cons '() s))
-  (cons (reverse rxs) s.remaining))
+  (let-values (((rx* s.remaining) (s-split-foldl n cons '() s)))
+    (values (reverse rx*) s.remaining)))
 
 (define (s-take/set/steps steps n s)
   (if (and n (= n 0)) (set)
@@ -50,7 +65,7 @@
 
 (define (s-take/set n s) (s-take/set/steps #f n s))
 (define (s-take     n s) (s-take/steps #f n s))
-(define (s-drop     n s) (cdr (s-foldl n (lambda (_ acc) #t) #t s)))
+(define (s-drop     n s) (let-values (((_ s) (s-split-foldl n (lambda (_ acc) #t) #t s))) s))
 
 ;; equivalent to (s-take #f s)
 (define (s->list s)
@@ -84,17 +99,25 @@
 (define (s-filter ? s) (s-foldr (lambda (x acc) (if (? x) (cons x acc) acc))
                                 '() s))
 
-(define (s-map f s . ss)
-  (cond ((null? s) '())
-        ((pair? s) (let loop ((ss-pending ss) (rss '()))
-                     (if (null? ss-pending)
-                       (let ((ss (reverse rss)))
-                         (cons (apply f (car s) (map car ss))
-                               (apply s-map f (cdr s) (map cdr ss))))
-                       (let next ((ss0 (car ss-pending)))
-                         (if (procedure? ss0) (thunk (next (ss0)))
-                           (loop (cdr ss-pending) (cons ss0 rss)))))))
-        (else      (thunk (apply s-map f (s) ss)))))
+(define s-map
+  (case-lambda
+    ((f s)
+     (let loop ((s s))
+       (cond ((null?      s) '())
+             ((procedure? s) (lambda () (loop (s))))
+             (else           (cons (f (car s)) (loop (cdr s)))))))
+    ((f s . s*)
+     (let loop.outer ((s s) (s* s*))
+       (cond ((null?      s) '())
+             ((procedure? s) (lambda () (loop.outer (s) s*)))
+             (else (let loop ((s*-pending s*) (rs* '()))
+                     (if (null? s*-pending)
+                         (let ((s* (reverse rs*)))
+                           (cons (apply f (car s) (map car s*))
+                                 (loop.outer (cdr s) (map cdr s*))))
+                         (let next ((s*0 (car s*-pending)))
+                           (cond ((procedure? s*0) (lambda () (next (s*0))))
+                                 (else (loop (cdr s*-pending) (cons s*0 rs*)))))))))))))
 
 ;; TODO: generalize to multiple streams
 (define (s-map/append f s)
@@ -104,21 +127,12 @@
 (define (s-each p s) (let ((s (s-force s)))
                        (unless (null? s) (p (car s)) (s-each p (cdr s)))))
 
-(define (s-foldl n f acc s)
-  (if (and n (= n 0)) (cons acc s)
-    (let ((s (s-force s)))
-      (if (null? s) (list acc)
-        (s-foldl (and n (- n 1)) f (f (car s) acc) (cdr s))))))
-
 (define (s-scan s acc f)
   (cons acc (cond ((null? s) '())
                   ((pair? s) (s-scan (cdr s) (f (car s) acc) f))
                   (else      (thunk (s-scan (s) acc f))))))
 
-(define (s-length s) (let loop ((s s) (l 0))
-                       (cond ((null? s) l)
-                             ((procedure? s) (loop (s) l))
-                             (else (loop (cdr s) (+ l 1))))))
+(define (s-length s) (s-foldl (lambda (_ l) (+ l 1)) 0 s))
 
 (define (s-group s ? @)
   (let ((@ (or @ (lambda (x) x))))
@@ -131,6 +145,35 @@
                           (else (let ((y (@ (car s))))
                                   (if (? y x) (loop (cons (car s) g) (cdr s))
                                     (cons g (next y s))))))))))))
+
+(define (s-chunk s len.chunk)
+  (cond
+    ((<= len.chunk 0) (error "chunk length must be positive" len.chunk))
+    ((=  len.chunk 1) (s-map vector s))
+    (else (let new ((s s))
+            (cond ((null?      s) '())
+                  ((procedure? s) (lambda () (new (s))))
+                  (else (let ((chunk (make-vector len.chunk)))
+                          (unsafe-vector*-set! chunk 0 (car s))
+                          (let loop ((s (cdr s)) (i 1))
+                            (cond
+                              ((null?      s) (list (vector-copy chunk 0 i)))
+                              ((procedure? s) (lambda () (loop (s) i)))
+                              (else (unsafe-vector*-set! chunk i (car s))
+                                    (let ((i (unsafe-fx+ i 1)))
+                                      (if (unsafe-fx< i len.chunk)
+                                          (loop (cdr s) i)
+                                          (cons chunk (new (cdr s)))))))))))))))
+
+(define (s-unchunk s)
+  (let next ((s s))
+    (cond ((null?      s) '())
+          ((procedure? s) (lambda () (next (s))))
+          (else (let* ((x* (car s)) (len.chunk (vector-length x*)))
+                  (let loop ((i 0))
+                    (if (unsafe-fx< i len.chunk)
+                        (cons (unsafe-vector*-ref x* i) (loop (unsafe-fx+ i 1)))
+                        (next (cdr s)))))))))
 
 (define (s-memo s)
   (cond ((procedure? s) (let ((v #f) (s s))
